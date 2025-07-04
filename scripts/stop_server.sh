79f53ac72e634efdca3a3bcb192a10d1a753f915
mgayathri181 <44227993+mgayathri181@users.noreply.github.com>
2025-07-04 00:12:05 -0400

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-isExistApp = `pgrep httpd`
-if [[ -n  $isExistApp ]]; then
-    service httpd stop        
-fi
+echo "Running stop_server.sh"
 
+isExistApp=$(pgrep httpd)
+if [[ -n "$isExistApp" ]]; then
+    echo "httpd process found. Stopping..."
+    sudo systemctl stop httpd
+else
+    echo "httpd not running. Nothing to stop."
+fi
